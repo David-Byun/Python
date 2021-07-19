1b5f3c3b0ded61a0adace1d55a30330e4bfb1c53
David-Byun <byundavid@naver.com>
2021-07-19 23:15:37 +0900

    Django Forms and CSRF!

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
+from django.views import View
 from django.shortcuts import render
+from . import forms
 
-# Create your views here.
+
+class LoginView(View):
+    def get(self, request):
+        form = forms.LoginForm()
+        return render(request, "users/login.html", {"form": form})
+
+    def post(self, request):
+        form = forms.LoginForm(request.POST)
+        print(form)
+
+
+""" 
+def login_view(request) :
+    if request.method == "GET":
+        pass
+    elif request.method == "P":
+        pass
+"""
