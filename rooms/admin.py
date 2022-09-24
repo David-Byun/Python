88a819d9d4b4334dab3513821be2ec96045b63d2
David-Byun <byundavid@naver.com>
2022-09-24 17:03:18 +0900

    #8.1 Search Fields ~ #8.2 Admin Actions

--- rooms/admin.py
+++ rooms/admin.py
@@ -3,8 +3,17 @@ from django.contrib import admin
 from .models import Room, Amenity
 
 
+@admin.action(description="Set all price to zero")
+def reset_prices(model_admin, request, rooms):
+    for room in rooms.all():
+        room.price = 0
+        room.save()
+
+
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
+
+    actions = (reset_prices,)
 
     list_display = (
         "name",
@@ -28,6 +37,8 @@ class RoomAdmin(admin.ModelAdmin):
         "created_at",
         "updated_at",
     )
+
+    search_fields = ("owner__username",)
 
     def total_amenities(self, room):
         return room.amenities.count()
