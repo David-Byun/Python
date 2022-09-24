0712487e47d50996712817d016512c0e4079f6da
David-Byun <byundavid@naver.com>
2022-09-24 13:05:19 +0900

    #8.0 Methods

--- config/settings.py
+++ config/settings.py
@@ -28,6 +28,11 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 # Application definition
+
+THIRD_PARTY_APPS = [
+    "rest_framework",
+]
+
 CUSTOM_APPS = [
     "common.apps.CommonConfig",
     "users.apps.UsersConfig",
@@ -50,7 +55,7 @@ SYSTEM_APPS = [
     "django.contrib.staticfiles",
 ]
 
-INSTALLED_APPS = CUSTOM_APPS + SYSTEM_APPS
+INSTALLED_APPS = SYSTEM_APPS + THIRD_PARTY_APPS + CUSTOM_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
