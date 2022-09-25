26a0c464d377a137030929ecd18e06f4dfc2b1d0
David-Byun <byundavid@naver.com>
2022-09-25 17:56:37 +0900

    #10.12 ModelViewSet

--- categories/views.py
+++ categories/views.py
@@ -3,11 +3,17 @@ from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
 from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 from .models import Category
 from .serializers import CategorySerializer
 
 
-class Categories(APIView):
+class CategoryViewSet(ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+
+''' class Categories(APIView):
     def get(self, request):
         all_categories = Category.objects.all()
         serializer = CategorySerializer(all_categories, many=True)
@@ -96,3 +102,4 @@ def categories(request):
             )
         else:
             return Response(serializer.errors) """
+ '''
