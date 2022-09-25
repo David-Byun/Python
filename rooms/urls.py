666b090d3daffaac7950cdfabad6b5291e9d2cd7
David-Byun <byundavid@naver.com>
2022-09-25 18:30:00 +0900

    #11.1 Amenity Detail

--- rooms/urls.py
+++ rooms/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.say_all_rooms),
-    path("<int:room_id>", views.see_one_room),
+    path("amenities/", views.Amenities.as_view()),
+    path(
+        "amenities/<int:pk>",
+        views.AmenityDetail.as_view(),
+    ),
 ]
