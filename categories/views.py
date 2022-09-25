79726affe7c87d1cc64e4bfc149ef7bfa805a4d6
David-Byun <byundavid@naver.com>
2022-09-25 11:14:07 +0900

    #10.5 is_valid()

--- categories/views.py
+++ categories/views.py
@@ -1,12 +1,26 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Category
+from .serializers import CategorySerializer
+from categories import serializers
 
 # Create your views here.
-@api_view()
+@api_view(["GET", "POST"])
 def categories(request):
-    return Response(
-        {
-            "ok": True,
-        }
-    )
+    if request.method == "GET":
+        all_categories = Category.objects.all()
+        serializer = CategorySerializer(all_categories, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            return Response({"created": True})
+        else:
+            return Response(serializer.errors)
+
+
+@api_view()
+def category(request, pk):
+    category = Category.objects.get(pk=pk)
+    serializer = CategorySerializer(category)
+    return Response(serializer.data)
